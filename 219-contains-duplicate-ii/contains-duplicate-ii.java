445829c86d0702a048745a8da004ba1d13d79471
PRITHIVIRAJ <148620809+Prithiviraj2005@users.noreply.github.com>
2025-09-08 22:47:10 +0530

    Time: 16 ms (99.32%) | Memory: 59.4 MB (81.80%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,12 +1,20 @@
-class Solution {
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j <= i + k && j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    return true;
-                }
-            }
+public class Solution
+{
+    public boolean containsNearbyDuplicate(int[] nums, int k) 
+    {
+        Set<Integer> window = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++)
+        {
+
+            if (i > k)
+                window.remove(nums[i - k - 1]);
+
+
+            if (!window.add(nums[i]))
+                return (true);
         }
-        return false;
+
+        return (false);
     }
 }
