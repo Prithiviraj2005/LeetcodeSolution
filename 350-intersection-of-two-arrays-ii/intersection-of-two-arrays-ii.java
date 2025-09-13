d8667a205cbf8933a95a43c1e4c5e39a85dac64c
PRITHIVIRAJ <148620809+Prithiviraj2005@users.noreply.github.com>
2025-09-13 21:23:45 +0530

    Time: 4 ms (27.59%) | Memory: 45.4 MB (5.23%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -3,6 +3,7 @@ class Solution {
         int l1 = nums1.length;
         int l2 = nums2.length;
         int i = 0, j = 0, k = 0;
+        int[] ans =new int[l1*l2];
         Arrays.sort(nums1);
         Arrays.sort(nums2);
         while( i < l1 && j < l2)
@@ -17,10 +18,10 @@ class Solution {
             }
             else
             {
-                nums1[k++] = nums1[i++];
+                ans[k++] = nums1[i++];
                 j++;
             }
         }
-        return Arrays.copyOfRange(nums1,0,k);
+        return Arrays.copyOfRange(ans,0,k);
     }
 }
