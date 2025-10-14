6095738acee26cd36e06379cb136634f6e1b2479
PRITHIVIRAJ <148620809+Prithiviraj2005@users.noreply.github.com>
2025-10-14 19:11:49 +0530

    Time: 7 ms (11.27%) | Memory: 42.6 MB (5.40%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -2,7 +2,8 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         String ans="";
 
-        Arrays.sort(strs);
+        Arrays.sort(strs);  //EG:toy tap tom
+                            //tap tom toy (first and last String only should compare)
         String first=strs[0];
         String last=strs[strs.length-1];
         for(int i=0;i<first.length();i++){
