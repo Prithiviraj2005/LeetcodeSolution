8bebddbee2e00fd6e50a4e67f83018dbc7488890
PRITHIVIRAJ <148620809+Prithiviraj2005@users.noreply.github.com>
2025-10-18 20:23:22 +0530

    Time: 5 ms (33.59%) | Memory: 45.2 MB (7.89%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -7,14 +7,16 @@ class Solution {
         for (int num : nums) {
             set.add(num);
             if (set.size() > 3) {
-                set.pollFirst();
+                set.pollFirst(); // safely removes and returns smallest (no exception)
             }
         }
+
+        // If less than 3 distinct numbers, return max
         if (set.size() < 3) {
             return set.last();
         }
 
-
+        // Else return the third maximum
         return set.first();
     }
 }
